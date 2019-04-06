1ab6f17604aef62b7ed69139520697bdd63ec8dd
Javier Palacios <javiplx@gmail.com>
2019-04-06 16:03:46 +0200

    fixit2

--- src/test/groovy/com/netflix/gradle/plugins/deb/DebPluginTest.groovy
+++ src/test/groovy/com/netflix/gradle/plugins/deb/DebPluginTest.groovy
@@ -178,7 +178,7 @@ class DebPluginTest extends ProjectSpec {
         'optional' == scan.getHeaderEntry('Priority')
 
         scan.controlContents['./conffiles'].eachLine {
-            '/etc/init.d/served' == it or '/opt/bleah/main/groovy' == it
+            '/etc/init.d/served' == it || '/opt/bleah/main/groovy' == it
         }
         
         def file = scan.getEntry('./a/path/not/to/create/alone')
